49a0c6d9ffebc9b9b5a6cd84bc9411e344b0a08f
popowell <popowell@ncsu.edu>
2016-04-18 11:19:10 -0400

    added bbfs log messages for qos_throttle calls

--- client/src/qos_throttle/bbfs/src/bbfs.c
+++ client/src/qos_throttle/bbfs/src/bbfs.c
@@ -342,6 +342,8 @@ int bb_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_
 	
 	qos_throttle(10000, QOS_READ_OPS);
 
+	log_msg("\nqos_throttle(10000, QOS_READ_OPS\n");
+
     return log_syscall("pread", pread(fi->fh, buf, size, offset), 0);
 }
 
@@ -367,6 +369,8 @@ int bb_write(const char *path, const char *buf, size_t size, off_t offset,
     log_fi(fi);
 	
 	qos_throttle(10000, QOS_WRITE_OPS);
+
+	log_msg("\nqos_throttle(10000, QOS_WRITE_OPS\n");
 
     return log_syscall("pwrite", pwrite(fi->fh, buf, size, offset), 0);
 }
