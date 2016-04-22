fa59cf12826f1964b10c2849303a85006e4cac8e
popowell <popowell@ncsu.edu>
2016-04-22 18:34:15 -0400

    merged qqfs with development, removed client-kernel files

---
+++ client/src/qqfs/src/qos_throttle.sh
@@ -0,0 +1,31 @@
+# FUSE - update & install
+
+sudo pacman -S build-essential git libfuse-dev libcurl4-openssl-dev libxml2-dev mime-support automake libtool
+
+sudo pacman -S install pkg-config libssl-dev
+
+#git clone https://github.com/s3fs-fuse/s3fs-fuse
+
+#cd s3fs-fuse/
+
+#sudo sh ./autogen.sh
+
+#sudo ./configure --prefix=/usr --with-openssl
+
+#sudo make
+
+#sudo make install
+
+#cd ../
+
+cd ../
+
+# End FUSE install
+# Install BBFS
+
+sudo sh autogen.sh
+
+sudo ./configure
+
+sudo make
+
