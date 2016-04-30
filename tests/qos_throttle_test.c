9cd9a6f13c7b8128c2602e9106a6c470eda2fa50
popowell <popowell@ncsu.edu>
2016-04-30 18:23:43 -0400

    fixed printf indentation, and unsigned int printf

--- tests/qos_throttle_test.c
+++ tests/qos_throttle_test.c
@@ -26,12 +26,12 @@ void test_token_bucket(void)
 
 	unsigned int time_diff = current_ts - rb.rb_ts;
 	
-	printf("\n%40stest_token_bucket() - time_diff = %lu\n", spacer, time_diff);
+	printf("\n%5stest_token_bucket() - time_diff = %u\n", spacer, time_diff);
 	
 	CU_ASSERT(time_diff < 1000000);
 	CU_ASSERT(qos_can_send(&rb) == 0);
 	
-	printf("\n%40stest_token_bucket() - tokens = %lu\n", spacer, rb.rb_tokens);
+	printf("\n%5stest_token_bucket() - tokens = %u\n", spacer, rb.rb_tokens);
 }
 
 void test_throttle(void)
@@ -44,13 +44,13 @@ void test_throttle(void)
 	
     rb.rb_ts = qos_get_uptime();
 	
-	printf("\n%40stest_throttle() - rb_ts = %lu\n", spacer, rb.rb_ts);
+	printf("\n%5stest_throttle() - rb_ts = %u\n", spacer, rb.rb_ts);
 
 	qos_throttle("/home/vagrant/QualiQueue/2016springTeam28/qqfs/example/mountdir/",1);
 
 	unsigned int uptime = qos_get_uptime() - rb.rb_ts;
 	
-	printf("%40stest_throttle() - uptime = %lu\n", spacer, uptime);
+	printf("%5stest_throttle() - uptime = %u\n", spacer, uptime);
 
 	CU_ASSERT(uptime > 900000 && uptime < 1100000);
 }
@@ -65,11 +65,11 @@ void test_update_tokens(void)
 	
     rb.rb_ts = qos_get_uptime();
 	
-	printf("\n%40stest_update_tokens() - rb_ts = %lu\n", spacer, rb.rb_ts);
+	printf("\n%5stest_update_tokens() - rb_ts = %u\n", spacer, rb.rb_ts);
 
 	update_tokens(&rb);
 	
-	printf("\n%40stest_update_tokens() - tokens = %lu\n", spacer, rb.rb_tokens);
+	printf("\n%5stest_update_tokens() - tokens = %u\n", spacer, rb.rb_tokens);
 
 	CU_ASSERT(rb.rb_tokens > 0);
 }
