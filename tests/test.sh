8c4834d2a65ba587d5191f6f7f41658d6599e7a8
popowell <popowell@ncsu.edu>
2016-04-30 22:45:04 -0400

    added shared_common to test

--- tests/test.sh
+++ tests/test.sh
@@ -4,10 +4,10 @@ cp ../qqfs/src/qos_throttle.h ./qqfs
 
 cd ./qqfs
 
-#cp ../client/src/main/shared_common.h .
+cp ../client/src/main/shared_common.h .
 
-#cp ../client/src/main/shared_common.c .
+cp ../client/src/main/shared_common.c .
 
-gcc -Wall -c qos_throttle.c -O0 -fprofile-arcs -ftest-coverage -g
+gcc -Wall -c qos_throttle.c shared_common.c -O0 -fprofile-arcs -ftest-coverage -g
 
-gcc -Wall -L/usr/local/lib -o qos_throttle_test qos_throttle_test.c qos_throttle.o -lcunit -pthread -lgcov
+gcc -Wall -L/usr/local/lib -o qos_throttle_test qos_throttle_test.c qos_throttle.o shared_common.o -lcunit -pthread -lgcov
