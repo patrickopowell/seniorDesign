206d48bda00e5810ae16fe8feab3dae58100d2d7
popowell <popowell@ncsu.edu>
2016-04-30 23:35:13 -0400

    merging main and logging from client-user

---
+++ client/src/main/main.c
@@ -0,0 +1,100 @@
+#include "main.h"
+
+const char *argp_program_version = "qqclient-0.1";
+const char *argp_program_bug_address = "<racampbe@ncsu.edu>";
+static char doc[] = "qqclient -- Instantiate QualiQueue on a location.";
+static char args_doc[] = "BASE_FOLDER EXPORT_FOLDER QQ_SERVER_IP";
+static struct argp_option options[] = {
+	{"debug", 'd', 0, 0, "Report debug messages."},
+	{0}
+};
+static struct argp argp = {options, parse_opt, args_doc, doc};
+
+int main(int argc, char *argv[])
+{
+	struct arguments arguments;
+	arguments.debug = 0;
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+	setup_clean_kill();
+	qos_setup_logging("qqclient");
+	int lockid = qos_setup_instance();
+	pthread_t threads[2];
+	if (pthread_create(&threads[0], NULL, &qos_receiver_start, NULL))
+		qos_log_critical("Could not create receiver thread.");
+	if (pthread_create(&threads[1], NULL, &qos_parser_start, NULL))
+		qos_log_critical("Could not create parser thread.");
+	for (int i = 0; i < 2; i++)
+		pthread_join(threads[i], NULL);
+	qos_halt_logging();
+	qos_destroy_instance(lockid);
+	qos_log_info("Exiting qqclient.");
+	return 0;
+}
+
+/**
+ * Setup clean kill hook on ctrl+c, etc.
+ * On sigaction, perform runhandler(int) method.
+ */
+void setup_clean_kill()
+{
+	struct sigaction act;
+	act.sa_handler = run_handler;
+	sigemptyset(&(act.sa_mask));
+	act.sa_flags = 0;
+	sigaction(SIGINT, &act, 0);
+}
+
+int qos_setup_instance()
+{
+	qos_log_info("Setting up qqclient instance.");
+	shr_init_mem();
+	int lockid = open(QQCLIENT_LOCK, O_WRONLY | O_CREAT, 0600);
+	int rc = flock(lockid, LOCK_EX | LOCK_NB);
+	if (rc == -1) {
+		qos_log_info("Existing qqclient instance running, inserting server.");
+		// add server
+		exit(1);
+	} else {
+		qos_log_info("No qqclient instance detected, starting.");
+	}
+	running = 1;
+	return lockid;
+}
+
+void qos_destroy_instance(int lockid)
+{
+	qos_log_info("Destroying qqclient instance.");
+	shr_close_mem();
+	flock(lockid, LOCK_UN);
+}
+
+/**
+ * Sets running to 0 to indicate threads should quit.
+ * Provides a clean death.
+ */
+void run_handler(int sig)
+{
+	running = 0;
+}
+
+static error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	struct arguments *arguments = state->input;
+	switch (key) {
+		case 'd':
+			arguments->debug = 1;
+			break;
+		case ARGP_KEY_ARG:
+			if (state->arg_num >= 3) // Too many args
+				argp_usage(state);
+			arguments->args[state->arg_num] = arg;
+			break;
+		case ARGP_KEY_END:
+			if (state->arg_num < 3) // Too few args
+				argp_usage(state);
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
---
+++ client/src/main/receiver/receiver.h
@@ -0,0 +1,22 @@
+/**
+ * CSC 492 - End-to-End Storage QoS
+ * Client User Mode
+ * @authors Remington Campbell
+ * In charge of sending and receiving data to the client kernel space module as well as to the QoS server located at the storage endpoint.
+ * Communicates to the client kernel module by [undetermined, syscall?].
+ * Communicates to the QoS server via TCP/IP connection
+ */
+
+#include "../common.h"
+#include "../../lib/qos-json/qos-json.h"
+
+#define LISTENPORT "15555"
+
+// Size of network send/receive buffers.
+#define BUFFERLENGTH 5000
+
+// Number of potential SLA connections to queue
+#define SLA_BACKLOG 5
+
+/** FORWARD DECLARATIONS **/
+void *qos_receiver_start(void *in);
