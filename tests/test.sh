9f6a0be8093a6ba790df77ca80ed09535193d9b3
popowell <popowell@ncsu.edu>
2016-05-01 03:44:30 -0400

    refactored to work with qqclient

--- tests/test.sh
+++ tests/test.sh
@@ -2,16 +2,16 @@ cp ../qqfs/src/qos_throttle.c ./qqfs
 
 cp ../qqfs/src/qos_throttle.h ./qqfs
 
-cp ../client/src/main/shared_common.h ./qqfs
+cp ../libcommon/client/communication.h ./qqfs
 
-cp ../client/src/main/shared_common.c ./qqfs
+cp ../libcommon/client/communication.c ./qqfs
 
-cp ../client/src/lib/logging/logging.h ./lib/logging
+cp ../libcommon/logging/logging.h ./libcommon/logging
 
-cp ../client/src/lib/logging/logging.c ./lib/logging
+cp ../libcommon/logging/logging.c ./libcommon/logging
 
 cd ./qqfs
 
-gcc -Wall -c qos_throttle.c shared_common.c ../lib/logging/logging.c -O0 -fprofile-arcs -ftest-coverage -g
+gcc -Wall -c qos_throttle.c communication.c ../libcommon/logging/logging.c -O0 -fprofile-arcs -ftest-coverage -g
 
 gcc -Wall -L/usr/local/lib -o qos_throttle_test qos_throttle_test.c qos_throttle.o shared_common.o logging.o -lcunit -pthread -lgcov
