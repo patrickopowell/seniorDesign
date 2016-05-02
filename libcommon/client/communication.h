e457e7b95483a2c48968a698b695cebd1328c57f
popowell <popowell@ncsu.edu>
2016-05-01 23:03:34 -0400

    fixed communication.h include

--- libcommon/client/communication.h
+++ libcommon/client/communication.h
@@ -8,7 +8,7 @@
 #include <sys/shm.h>
 #include <fcntl.h>
 #include <semaphore.h>
-#include "../logging.h"
+#include "../logging/logging.h"
 
 #define COM_SLA_MEM "/qualiqueue/sla"
 #define COM_STAT_MEM "/qualiqueue/stats"
