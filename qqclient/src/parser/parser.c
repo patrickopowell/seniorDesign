d96b0a597026b8fd260a69139dfe55860fc6d4fc
Remington Campbell <racampbe@ncsu.edu>
2016-05-02 17:03:32 -0400

    Testing Parsing

--- qqclient/src/parser/parser.c
+++ qqclient/src/parser/parser.c
@@ -3,10 +3,28 @@
 void *qq_parser_start(void *in)
 {
 	qq_log_info("Starting Parser.");
-	while(check_running() != 0)
+	while(check_running() != 0) {
 		usleep(1000000);
+		qq_lock();
+		int num_servers = qqfs_instance_list->count;
+		qq_unlock();
+		struct qqfs_instance *curr_instance = calloc(1, sizeof(struct qqfs_instance));
+		struct stat_info *curr_stat = calloc(1, sizeof(struct stat_info));
+		for (int i = 0; i < num_servers; i++) {
+			qq_get_qqfs_instance_by_idx(i, curr_instance)
+			com_get_stat(curr_instance->export_path, curr_stat);
+			qq_parse_stat(&curr_stat);
+		}
+		free(curr_instance);
+		free(curr_stat);
+	}
 	qq_log_info("Exiting Parser.");
 	return 0;
+}
+
+void qq_parse_stat(struct stat_info *stat)
+{
+	printf("%d %d\n%s\n%d %d %d %d %d", stat->s_dev, stat->i_ino, stat->path, stat->iops_sec, stat->reads_queued, stat->writes_queued, stat->iops_suspended, stat->throughput);
 }
 
 /**
