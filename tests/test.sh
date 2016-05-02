addbe32da4c76c89e2b47ed8e3b5ef0efa752c18
popowell <popowell@ncsu.edu>
2016-05-02 12:41:33 -0400

    fixed directory on test script

--- tests/test.sh
+++ tests/test.sh
@@ -6,12 +6,12 @@ cp ../libcommon/client/communication.h ./qqfs
 
 cp ../libcommon/client/communication.c ./qqfs
 
-cp ../libcommon/logging/logging.h ./libcommon/logging
+cp ../libcommon/logging/logging.h ./logging
 
-cp ../libcommon/logging/logging.c ./libcommon/logging
+cp ../libcommon/logging/logging.c ./logging
 
 cd ./qqfs
 
-gcc -Wall -c qos_throttle.c communication.c ../libcommon/logging/logging.c -O0 -fprofile-arcs -ftest-coverage -g
+gcc -Wall -c qos_throttle.c communication.c ../logging/logging.c -O0 -fprofile-arcs -ftest-coverage -g
 
 gcc -Wall -L/usr/local/lib -o qos_throttle_test qos_throttle_test.c qos_throttle.o communication.o logging.o -lcunit -pthread -lgcov
