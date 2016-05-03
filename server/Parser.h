002449d99412f5b1e60b590fac1aaebbe50a2b17
Zwick Shaw <zashaw@ncsu.edu>
2016-05-03 03:47:51 -0400

    I just made some minor changes to my parser.

---
+++ server/Parser.h
@@ -0,0 +1,10 @@
+char *getVal(char *, char *);
+long getClientID(char *);
+long getProtocol(char *);
+long getVersion(char *);
+long getStorageType(char *);
+long getIopsMax(char *);
+long getIopsMin(char *);
+long getThroughputMax(char *);
+long getThroughputMin(char *);
+long getUnusedBandwidth(char *);
