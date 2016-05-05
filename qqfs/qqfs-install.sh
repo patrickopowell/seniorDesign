8400d72ae37f5aab12b32f7233bf6c32520c97db
popowell <popowell@ncsu.edu>
2016-05-05 00:27:46 -0400

    removed fuse install

--- qqfs/qqfs-install.sh
+++ qqfs/qqfs-install.sh
@@ -4,27 +4,6 @@ echo ""
 echo "---- Installing QQFS ----"
 echo ""
 
-#### FUSE - update & install
-
-sudo pacman -S build-essential git libfuse-dev libcurl4-openssl-dev libxml2-dev mime-support automake libtool
-
-sudo pacman -S install pkg-config libssl-dev
-
-git clone https://github.com/s3fs-fuse/s3fs-fuse
-
-cd s3fs-fuse/
-
-sudo bash ./autogen.sh
-
-sudo ./configure --prefix=/usr --with-openssl
-
-sudo make
-
-sudo make install
-
-cd ../
-
-#### End FUSE install
 #### Install QQFS
 
 sudo bash ./autogen.sh
